589a1bec9ad4a370cd94d0875e2515cf78df7dcb
copicogithub1 <copicocareers@gmail.com>
2017-11-13 18:24:59 +0000

    Fix reward denomination

--- src/main.cpp
+++ src/main.cpp
@@ -48,7 +48,7 @@ unsigned int nModifierInterval = 3 * 60; // time to elapse before new modifier i
 
 
 // Block at which COINBASE_MATURITY_NEW comes into effect
-int COINBASE_MATURITY_SWITCH = 24961;
+int COINBASE_MATURITY_SWITCH = 25770;
 
 //Coinbase maturity after block 1011
 int COINBASE_MATURITY_NEW = 20;
@@ -1183,11 +1183,11 @@ int64_t GetProofOfStakeReward(int nHeight, int64_t nCoinAge, int64_t nFees)
 	{
 		nSubsidy = nCoinAge * COIN_YEAR_REWARD * 33 / (365 * 33 + 8);
 	}
-	else if (nBestHeight <= 26880)
+	else if (nBestHeight <= 25770)
 	{
 		nSubsidy = 15;
 	}
-	else if (nBestHeight <= 25500)
+	else if (nBestHeight <= 26880)
 	{
 		nSubsidy = 15 * COIN;
 	}
